c2f48c79165b77a58d4face2f846134a31a99125
David Hileman <dhileman823@gmail.com>
2020-03-15 19:26:29 -0400

    added get/post orgs pages

--- js/api.js
+++ js/api.js
@@ -2,11 +2,13 @@ var api = {
 
     apiAddress: "http://192.168.1.121:5000",
 
-    apiCall: function (url, method, data, callback){
+    apiCallSend: function (url, method, data, callback){
+        console.log("apiCall - POST: " + JSON.stringify(data));
         $.ajax({
             url: url,
             method: method,
-            data: data,
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(data),
             success: function(response){
                 callback(response)
             }
@@ -25,12 +27,12 @@ var api = {
     
     createUser: function(user, callback){
         var url = this.apiAddress + "/users"
-        this.apiCall(url, "POST", org, callback);
+        this.apiCallSend(url, "POST", org, callback);
     },
     
     updateUser: function(user, callback){
         var url = this.apiAddress + "/users/"+user.userId;
-        this.apiCall(url, "POST", org, callback);
+        this.apiCallSend(url, "POST", org, callback);
     },
     
     getUser: function(userId, callback){
@@ -44,8 +46,8 @@ var api = {
     },
     
     createOrg: function(org, callback){
-        var url = this.apiAddress + "/orgs"
-        this.apiCall(url, "POST", org, callback);
+        var url = this.apiAddress + "/organizations"
+        this.apiCallSend(url, "POST", org, callback);
     },
     
     getOrg: function(orgId, callback){
