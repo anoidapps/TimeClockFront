51819ee2419d176d0645c579f35ca9d211853af4
David Hileman <dhileman823@gmail.com>
2020-03-20 21:44:11 -0400

    fixed job list and added user list

--- js/controllers/user-list.js
+++ js/controllers/user-list.js
@@ -4,14 +4,14 @@ $( document ).ready(function() {
     //get the org id from query parameter
     var orgId = util.getParameterByName("org-id");
     if(orgId){
-        //get all jobs by org id on page load
-        api.getJobs(orgId, function(response){
+        //get all users by org id on page load
+        api.getUsers(orgId, function(response){
             console.log(response)
-            var jobs = JSON.parse(response);
+            var users = JSON.parse(response);
             var html = "<ul>";
             //loop through the response and build the list of orgs
-            for(var i=0; i<jobs.length; i++){
-                html += "<li><a href=\"job-home.html?job-id="+jobs[i].job_id+"\">" + jobs[i].name+"</a></li>";
+            for(var i=0; i<users.length; i++){
+                html += "<li><a href=\"user-home.html?user-id="+users[i].user_id+"\">" + users[i].first_name+" " + users[i].first_name + "</a></li>";
             }
             html += "</ul>";
             //show the list in the div placeholder
